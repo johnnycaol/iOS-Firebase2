dbfcfc647b2c8105327b46e97387c47b5f93fdf9
Johnny Cao <johnny@soapboxhq.com>
2016-06-23 10:43:42 -0400

    Initial Commit

---
+++ quick-chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quick-chat
+//
+//  Created by Luchao Cao on 2016-06-23.
+//  Copyright © 2016 Luchao Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
